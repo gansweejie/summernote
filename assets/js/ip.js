47902f3037191147a0759a8907185e060bdd697d
gansweejie <gansweejie2001@gmail.com>
2022-11-22 10:48:29 +0800

    Renew

--- assets/js/ip.js
+++
@@ -1,3 +0,0 @@
-var address = "https://tianz.ordomalaysia.com/api/";
-var api_key = "wcMfc6tfNajPQiXwR3OZyTmcT9XUvAAEhQ9SOKNOaCiE7fiYyeo66VOcKBWtXUhD";
-var apiHeader = { headers: { 'Content-Type': 'multipart/form-data' } };
